268105ae1e69c9fc4e968db880c7263462560341
eduardo-zucco <eduardozucco89@gmail.com>
2025-05-27 14:00:08 -0300

    comeco a implementar metodo de imprimir o tabuleiro

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -9,15 +9,15 @@ namespace tabuleiro
         public int qteMovimentos { get; protected set; }
         public Tabuleiro tab {  get; protected set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
         {
             this.posicao = posicao;
-            this.cor = cor;
             this.tab = tab;
+            this.cor = cor;
             this.qteMovimentos = 0;
         }
 
-
+        
 
 
     }
