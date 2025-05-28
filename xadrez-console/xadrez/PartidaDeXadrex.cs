b457ecd3550ffec7a0aac2695f6fd412489766b3
eduardo-zucco <eduardozucco89@gmail.com>
2025-05-28 15:41:24 -0300

    teste de erro

--- xadrez-console/xadrez/PartidaDeXadrex.cs
+++ xadrez-console/xadrez/PartidaDeXadrex.cs
@@ -66,8 +66,20 @@ namespace xadrez
                 xeque= false;
             }
 
+            if (testeXequeMate(adversaria(jogadorAtual)))
+            {
+                terminada = true;
+
+            }
+            else
+            {
                 turno++;
-            mudaJogador();
+                mudaJogador();
+            }
+
+
+
+              
         }
 
         public void validarPosicaoDeOrigem (Posicao pos)
@@ -170,6 +182,37 @@ namespace xadrez
                 }
             }
             return false;
+        }
+
+        public bool testeXequeMate (Cor cor)
+        {
+            if (!estaEmXeque(cor))
+            {
+                return false;
+            }
+            foreach(Peca x in pecasEmJogo(cor))
+            {
+                bool[,] mat = x.movimentosPossiveis();
+                for (int i=0; i<tab.linhas; i++)
+                {
+                    for (int j=0; j<tab.colunas; j++)
+                    {
+                        if (mat[i, j])
+                        {
+                            Posicao destino = new Posicao(i, j);
+                            Peca pecaCapturada = executaMovimento(x.posicao, destino);
+                            bool testeXeque = estaEmXeque(cor);
+                            desfazMovimento(x.posicao, destino, pecaCapturada);
+                            if (!testeXeque)
+                            {
+                                return false;
+                            }
+                        }
+
+                    }
+                }
+            }
+            return true;
         }
 
         public void colocarNovaPeca (char coluna, int linha, Peca peca)
